a0b63938fce4e9fa666b9c8c2173e836f96b7182
kateusacova <kateusacova@gmail.com>
2022-11-18 18:07:50 +0000

    Configured postgres

---
+++ src/ormconfig.ts
@@ -0,0 +1,17 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+const environment = process.env.NODE_ENV || 'development';
+const data: any = dotenv.parse(fs.readFileSync(`${environment}.env`));
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: data.DB_USERNAME,
+  password: data.DB_PASSWORD,
+  database: 'mediumclone'
+};
+
+export default config;
